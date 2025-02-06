8ece00f939ddbdda3ba970fdfd77cc06f4474d59
Anik <sheikhanikbd@gmail.com>
2025-02-06 15:58:52 +0600

    fix

--- src/lib/menuData.ts
+++ src/lib/menuData.ts
@@ -161,7 +161,7 @@ export const menuItems = [
           label: "Logout",
           href: "/logout",
           // visible: ["admin", "teacher", "student", "parent"],
-          visible: [SCHOOL_ADMIN, TEACHER, STUDENT, PARENT],
+          visible: [CENTRAL_ADMIN, SCHOOL_ADMIN, TEACHER, STUDENT, PARENT],
         },
       ],
     },
