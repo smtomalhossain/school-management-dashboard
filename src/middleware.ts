457ef103e1e21b1e0304ae1dd1a46a5966bd2b14
Tomal Hossain <tomalhossain84@gmail.com>
2025-02-09 13:48:48 +0600

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,41 +4,41 @@ import { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
     console.log('------------------' + req.nextUrl.pathname);
 
-    var isAuthenticated = false;
-    // check if authenticated
-    try {
-        const res = await fetch("http://localhost:3001/api/authentication/get-user",
-            {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json",
-                    Cookie: req.headers.get("cookie") || "",
-                }
-            }
-        );
+    // var isAuthenticated = false;
+    // // check if authenticated
+    // try {
+    //     const res = await fetch("http://localhost:3001/api/authentication/get-user",
+    //         {
+    //             method: "GET",
+    //             credentials: "include",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 Cookie: req.headers.get("cookie") || "",
+    //             }
+    //         }
+    //     );
 
-        if (res.status === 200) {
-            isAuthenticated = true;
-        }
-    } catch (error) {
-        console.log(error);
-    }
+    //     if (res.status === 200) {
+    //         isAuthenticated = true;
+    //     }
+    // } catch (error) {
+    //     console.log(error);
+    // }
 
-    const isLoginRoute = req.nextUrl.pathname === "/login";
-    const isBaseRoute = req.nextUrl.pathname === "/";
+    // const isLoginRoute = req.nextUrl.pathname === "/login";
+    // const isBaseRoute = req.nextUrl.pathname === "/";
 
-    if (isAuthenticated && (isLoginRoute || isBaseRoute)) {
-        return NextResponse.redirect(new URL("/admin", 'http://localhost:3000'));
-    }
+    // if (isAuthenticated && (isLoginRoute || isBaseRoute)) {
+    //     return NextResponse.redirect(new URL("/admin", 'http://localhost:3000'));
+    // }
 
-    console.log("isAuthenticated", isAuthenticated);
-    console.log("isLoginRoute", isLoginRoute);
-    if (!isAuthenticated && !isLoginRoute) {
-        return NextResponse.redirect(new URL("/login", "http://localhost:3000"));
-    }
+    // console.log("isAuthenticated", isAuthenticated);
+    // console.log("isLoginRoute", isLoginRoute);
+    // if (!isAuthenticated && !isLoginRoute) {
+    //     return NextResponse.redirect(new URL("/login", "http://localhost:3000"));
+    // }
 
-    return NextResponse.next();
+    // return NextResponse.next();
 
 }
 
